cd239692e44837326de85be29f8993c75a2baa1f
Paige <paige.vogenthaler@gmail.com>
2016-12-01 20:14:10 -0800

    Create link and user schema, add todos to link and user models and request handler

--- app/config.js
+++ app/config.js
@@ -1,40 +1,14 @@
-var path = require('path');
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  },
-  useNullAsDefault: true
-});
-var db = require('bookshelf')(knex);
+var mongoose = require('mongoose');
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+// Opens connection to shortlydb in local running instance of mongodb
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+mongoose.connect('mongodb://localhost/shortlydb');
+
+var db = mongoose.connection;
+
+// Notifies us of successful or unsuccessful connections
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.on('open', console.log('We\'re connected.'));
 
 module.exports = db;
--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,28 @@
-var db = require('../config');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var mongoose = requre('mongoose');
+var Schema = mongoose.Schema;
+
+
+var linkSchema = new Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number
 });
+
+var Link = mongoose.model('Link', linkSchema);
+
+// linkSchema.pre('save', function() {}...)
+
+// todo: refactor old fn below into above
+
+//    function(model, attrs, options) {
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
 
 module.exports = Link;
